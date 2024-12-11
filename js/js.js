48010a98989b7f87cbc1cab28a741ebfdf7a9f99
gemyzero <mohamed0110gemy@gmail.com>
2024-12-11 20:01:34 +0200

    Test

--- js/js.js
+++ js/js.js
@@ -28,7 +28,7 @@
         country:"SA",
         city: city
     }
-  axios.get('http://api.aladhan.com/v1/timingsByCity', {
+  axios.get('https://api.aladhan.com/v1/timingsByCity', {
       params:param
     })
     .then(function (response) {
